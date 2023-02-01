1c4f2b235cb1c02455efc7dfafdb794ffb9059a5
Alex Panaev <panaev.as@gmail.com>
2023-02-01 22:13:32 +0300

    finished LoginTest class

--- src/test/java/aqa/LoginTest.java
+++ src/test/java/aqa/LoginTest.java
@@ -1,5 +1,7 @@
 package aqa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
@@ -8,8 +10,52 @@ public class LoginTest extends BaseTest{
     @Test
     public void createAccountTest(){
         new LoginPage(driver).open()
-                .fillInUserName("surnamealeks-rwdg@force.com")
-                .fillInPassword("Zx!cvbnm23+")
-                .submitForm();
+                            .fillInUserName("surnamealeks-rwdg@force.com")
+                            .fillInPassword("Zx!cvbnm23+")
+                            .submitForm();
+        //   //span[@class='slds-var-p-right_x-small']
+        driver.get("https://home34.lightning.force.com/lightning/o/Account/list?filterName=Recent");
+        final By accountTabLocatorXpath = By.xpath("//span[@class='slds-var-p-right_x-small']");
+        wait.until(ExpectedConditions.
+                visibilityOfElementLocated(accountTabLocatorXpath));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
