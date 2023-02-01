9699f1584fd285039517af84f9f6f2b254d525fd
Alex Panaev <panaev.as@gmail.com>
2023-02-01 21:09:37 +0300

    added first method into LoginTest class

--- src/test/java/aqa/LoginTest.java
+++ src/test/java/aqa/LoginTest.java
@@ -1,11 +1,15 @@
 package aqa;
 
 import org.testng.annotations.Test;
+import pages.LoginPage;
 
 public class LoginTest extends BaseTest{
 
     @Test
     public void createAccountTest(){
-
+        new LoginPage(driver).open()
+                .fillInUserName("surnamealeks-rwdg@force.com")
+                .fillInPassword("Zx!cvbnm23+")
+                .submitForm();
     }
 }
