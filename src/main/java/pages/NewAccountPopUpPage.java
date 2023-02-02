7eb670702936c7d54bab7971cfcefa28ee20e589
Alex Panaev <panaev.as@gmail.com>
2023-02-02 14:51:31 +0300

    added Wrappers package with empty wrapper Input class

--- src/main/java/pages/NewAccountPopUpPage.java
+++ src/main/java/pages/NewAccountPopUpPage.java
@@ -1,2 +1,10 @@
-package pages;public class NewAccountPopUp {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class NewAccountPopUpPage extends BasePage{
+    public NewAccountPopUpPage(WebDriver driver) {
+        super(driver);
+    }
 }
+//        // //span[text()='Account Name']//ancestor::div[contains(@class,'uiInput')]//input
