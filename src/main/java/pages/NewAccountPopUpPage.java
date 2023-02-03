d19e5677c1e26a5c552524d33f2a101cfe6e0c7f
Alex Panaev <panaev.as@gmail.com>
2023-02-04 02:25:36 +0300

    added createNewAccount() with new Input into NewAccountPopUpPage

--- src/main/java/pages/NewAccountPopUpPage.java
+++ src/main/java/pages/NewAccountPopUpPage.java
@@ -1,9 +1,17 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import pages.wrappers.Input;
 
 public class NewAccountPopUpPage extends BasePage{
     public NewAccountPopUpPage(WebDriver driver) {
         super(driver);
+    }
+
+    public NewAccountPopUpPage createNewAccount(String accountName){
+        new Input(driver, "Account Name").fillIn(accountName);
+
+        return this;
+
     }
 }
