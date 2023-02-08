c54ef7b9a36e1fec562c1759166f024feb584e59
Alex Panaev <panaev.as@gmail.com>
2023-02-08 18:49:00 +0400

    added select() into DropDown class

--- src/main/java/pages/wrappers/DropDown.java
+++ src/main/java/pages/wrappers/DropDown.java
@@ -1,5 +1,6 @@
 package pages.wrappers;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DropDown {
@@ -10,5 +11,13 @@ public class DropDown {
     public DropDown(WebDriver driver, String label) {
         this.driver = driver;
         this.label = label;
+    }
+    //span[text()='%s']//ancestor::div[contains(@class,'uiInput')]//input
+    public DropDown select(String option){
+        By fullLocator = By.xpath(String.format
+                                       ("//span[text()='%s']//ancestor::div[contains(@class,'uiInput')]//a", label));
+        driver.findElement(fullLocator).click();
+        driver.findElement(By.xpath("//a[@title='Competitor']"));
+        return this;
     }
 }
