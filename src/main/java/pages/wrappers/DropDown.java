7f096ebb2b8585e9c602e2ab8543fb3888a6c7b2
Alex Panaev <panaev.as@gmail.com>
2023-02-08 16:46:53 +0400

    added Constructor into DropDown class

--- src/main/java/pages/wrappers/DropDown.java
+++ src/main/java/pages/wrappers/DropDown.java
@@ -1,4 +1,14 @@
 package pages.wrappers;
 
+import org.openqa.selenium.WebDriver;
+
 public class DropDown {
+
+    WebDriver driver;
+    String label;
+
+    public DropDown(WebDriver driver, String label) {
+        this.driver = driver;
+        this.label = label;
+    }
 }
